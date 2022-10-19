14c22bc3538da14c54ff01874ab260969f6e7daf
vidhisanodiya15 <114800338+vidhisanodiya15@users.noreply.github.com>
2022-10-19 18:57:38 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+first=int(input("Enter the first number"))
+operator =(input("Enter the operator"))
+
+second =int(input("Enter the second number"))
+
+if operator =="+":
+ print("first+second")
+elif operator=="-":
+    print("first-second")
+elif operator=="*":
+    print("first*second")
+elif operator=="/":
+    print("first/second")
+else:
+    print("invalid number")
